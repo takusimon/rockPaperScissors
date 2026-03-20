ae581d236b87bf5ba0a4298ff62e70b46f73c18c
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-03-20 15:04:25 -0500

    Rock,Paper,Scissors
    Full and complete game of rock,paper,scissors

--- js/rockPaperScisssors.js
+++ js/rockPaperScisssors.js
@@ -1,56 +1,108 @@
 // FUNCTION getHumanChoice ()
+function getHumanChoice () {
     //asks the users for their choice and then return that choice
-        //INITIALIZE varChoice
-        //GET choice (rock,paper,scissors),SET varChoice
-        //RETURN varChoice
+        //INITIALIZE userChoice
+        let userChoice;
+        //GET choice (rock,paper,scissors),SET userChoice
+        userChoice = prompt('Enter rock,paper or scissors: ').toLowerCase();
+        //RETURN userChoice
+        return userChoice;
 // END FUNCTION
-
+}
 // FUNCTION getComputerChoice ()
+function getComputerChoice () {
     // returns the computers choice randomly selected
         // SET computerOptions as [rock,paper,scissors]
+        const computerOptions = ['rock','paper','scissors'];
         //INITIALIZE variable computerChoice
-        //RAND SELECT computerOPtions,SET computerChoice 
+        let computerChoice;
+        //RAND SELECT computerOPtions,SET computerChoice
+        let i = Math.floor(Math.random() * computerOptions.length);
+        computerChoice =  computerOptions[i];
         // RETURN computerChoice
+        return computerChoice;
 // END FUNCTION
-
+}
 // FUNCTION playGame (rounds)
+function playGame (rounds) {
     //INITIALIZE humanScore = 0
+    let humanScore = 0;
     //INITIALIZE computerScore = 0
+    let computerScore = 0;
     //FUNCTION playRound(humanChoice, computerChoice)
+    function playRound(humanChoice, computerChoice){
         //IF humanChoice == computerChoice THEN
-            //PRINT "Draw"
+        if (humanChoice === computerChoice) {
+            //RETURN "DRAW"
+            return "DRAW";
+        }
         //ELSE IF humanChoice == "rock" AND computerChoice == "scissors" THEN
-            // humanScore ++
-            //PRINT "User wins this round"
+        else if (humanChoice === "rock" && computerChoice === "scissors"){
+            //RETURN "HUMAN"
+            return "HUMAN";
+        }
         //ELSE IF humanChoice == "paper" AND computerChoice == "rock" THEN
-            // humanScore ++
-            //PRINT "User wins this round"
+        else if (humanChoice === "paper" && computerChoice === "rock"){
+            // RETURN "HUMAN"
+            return "HUMAN";
+        }
         //ELSE IF humanChoice == "scissors" AND computerChoice == "paper" THEN
-            // humanScore ++
-            //PRINT "User wins this round"
+        else if (humanChoice === "scissors" && computerChoice === "paper"){
+            //RETURN "HUMAN"
+            return "HUMAN";
+        }
         //ELSE
-            //computerScore ++
-            //PRINT "Computer wins this round"
+        else {
+            //RETURN "COMPUTER"
+            return "COMPUTER";
         //END IF
+        }
     //END FUNCTION
+    }
 
-    //FOR I <= rounds
+    //FOR i FROM 1 TO rounds
+    for (let i = 1; i <= rounds;i++) {
         //SET humanChoice = CALL getHumanChoice()
-        //SET computerChoice = CALL getComputer()
-        //CALL playRound(humanChoice,computerChoice)
+        const humanChoice = getHumanChoice();
+        //SET computerChoice = CALL getComputerChoice()
+        const computerChoice = getComputerChoice()
+        //INITIALIZE result = CALL playRound(humanChoice,computerChoice)
+        let result = playRound(humanChoice,computerChoice);
+        //IF result == "HUMAN" THEN
+        if (result === "HUMAN") {
+            //humanScore ++
+            humanScore++;
+        }
+        //ELSE IF result == "COMPUTER" THEN
+        else if (result === "COMPUTER"){
+            //computerScore ++
+            computerScore++;
+        }
+        //END IF
     //END FOR
+    }
 
     // IF humanScore == computerScore THEN
+    if (humanScore === computerScore){
         //PRINT "It's a Draw no winner"
+        console.log("It's a Draw");
+    }
     // ELSE IF humanScore > computerScore THEN
+    else if (humanScore > computerScore){
         //PRINT "USER wins!"
+        console.log("User wins!");
+    }
     //ELSE
+    else {
         //PRINT "Computer wins!"
+        console.log("Computer wins!");
     //END IF
+    }
 //END FUNCTION
+}
 
-//CALL playRound(x)
-
+//CALL playGame(x)
+playGame(5);
 
 
    
