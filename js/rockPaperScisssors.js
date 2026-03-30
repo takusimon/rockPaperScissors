7efd6af3129c338e078881e5b23aaf132db87b09
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-03-30 12:52:16 -0500

    Running Score
    refactored code, added buttons instead of user input
    displaying a live score

--- js/rockPaperScisssors.js
+++ js/rockPaperScisssors.js
@@ -1,14 +1,3 @@
-// FUNCTION getHumanChoice ()
-function getHumanChoice () {
-    //asks the users for their choice and then return that choice
-        //INITIALIZE userChoice
-        let userChoice;
-        //GET choice (rock,paper,scissors),SET userChoice
-        userChoice = prompt('Enter rock,paper or scissors: ').toLowerCase();
-        //RETURN userChoice
-        return userChoice;
-// END FUNCTION
-}
 // FUNCTION getComputerChoice ()
 function getComputerChoice () {
     // returns the computers choice randomly selected
@@ -23,86 +12,77 @@ function getComputerChoice () {
         return computerChoice;
 // END FUNCTION
 }
-// FUNCTION playGame (rounds)
-function playGame (rounds) {
-    //INITIALIZE humanScore = 0
-    let humanScore = 0;
-    //INITIALIZE computerScore = 0
-    let computerScore = 0;
-    //FUNCTION playRound(humanChoice, computerChoice)
-    function playRound(humanChoice, computerChoice){
-        //IF humanChoice == computerChoice THEN
-        if (humanChoice === computerChoice) {
-            //RETURN "DRAW"
-            return "DRAW";
-        }
-        //ELSE IF humanChoice == "rock" AND computerChoice == "scissors" THEN
-        else if (humanChoice === "rock" && computerChoice === "scissors"){
-            //RETURN "HUMAN"
-            return "HUMAN";
-        }
-        //ELSE IF humanChoice == "paper" AND computerChoice == "rock" THEN
-        else if (humanChoice === "paper" && computerChoice === "rock"){
-            // RETURN "HUMAN"
-            return "HUMAN";
-        }
-        //ELSE IF humanChoice == "scissors" AND computerChoice == "paper" THEN
-        else if (humanChoice === "scissors" && computerChoice === "paper"){
-            //RETURN "HUMAN"
-            return "HUMAN";
-        }
-        //ELSE
-        else {
-            //RETURN "COMPUTER"
-            return "COMPUTER";
-        //END IF
-        }
-    //END FUNCTION
+//INITIALIZE humanScore = 0
+let humanScore = 0;
+//INITIALIZE computerScore = 0
+let computerScore = 0;
+//FUNCTION playRound(humanChoice, computerChoice)
+function playRound(humanChoice, computerChoice){
+    //IF humanChoice == computerChoice THEN
+    if (humanChoice === computerChoice) {
+        //RETURN "DRAW"
+        return "DRAW";
     }
-
-    //FOR i FROM 1 TO rounds
-    for (let i = 1; i <= rounds;i++) {
-        //SET humanChoice = CALL getHumanChoice()
-        const humanChoice = getHumanChoice();
-        //SET computerChoice = CALL getComputerChoice()
-        const computerChoice = getComputerChoice()
-        //INITIALIZE result = CALL playRound(humanChoice,computerChoice)
-        let result = playRound(humanChoice,computerChoice);
-        //IF result == "HUMAN" THEN
-        if (result === "HUMAN") {
-            //humanScore ++
-            humanScore++;
-        }
-        //ELSE IF result == "COMPUTER" THEN
-        else if (result === "COMPUTER"){
-            //computerScore ++
-            computerScore++;
-        }
-        //END IF
-    //END FOR
+    //ELSE IF humanChoice == "rock" AND computerChoice == "scissors" THEN
+    else if (humanChoice === "rock" && computerChoice === "scissors"){
+        //RETURN "HUMAN"
+        return "HUMAN";
     }
-
-    // IF humanScore == computerScore THEN
-    if (humanScore === computerScore){
-        //PRINT "It's a Draw no winner"
-        console.log("It's a Draw");
+    //ELSE IF humanChoice == "paper" AND computerChoice == "rock" THEN
+    else if (humanChoice === "paper" && computerChoice === "rock"){
+        // RETURN "HUMAN"
+        return "HUMAN";
     }
-    // ELSE IF humanScore > computerScore THEN
-    else if (humanScore > computerScore){
-        //PRINT "USER wins!"
-        console.log("User wins!");
+    //ELSE IF humanChoice == "scissors" AND computerChoice == "paper" THEN
+    else if (humanChoice === "scissors" && computerChoice === "paper"){
+        //RETURN "HUMAN"
+        return "HUMAN";
     }
     //ELSE
     else {
-        //PRINT "Computer wins!"
-        console.log("Computer wins!");
+        //RETURN "COMPUTER"
+        return "COMPUTER";
     //END IF
     }
 //END FUNCTION
+};
+function winner (outcome) {
+    if (outcome === "HUMAN") {
+        humanScore ++;
+    } else if (outcome === "COMPUTER") {
+        computerScore ++;
+    }
 }
+function updateDisplay() {
+    humanDisplayedScore.textContent = `Human: ${humanScore}`;
+    computerDisplayedScore.textContent = `Computer: ${computerScore}`;
+}
+const resultDiv = document.querySelector('.results')
+const userChoiceRock = document.querySelector(".rock");
+userChoiceRock.addEventListener('click',() => {
+   const result = playRound('rock',getComputerChoice());
+   winner(result);
+   updateDisplay();
+});
 
-//CALL playGame(x)
-playGame(5);
+const userChoicePaper = document.querySelector('.paper');
+userChoicePaper.addEventListener('click', () => {
+    const result = playRound('paper',getComputerChoice());
+    winner(result);
+    updateDisplay();
+});
+
+const userChoiceScissors = document.querySelector('.scissors');
+userChoiceScissors.addEventListener('click', () => {
+    const result = playRound('scissors',getComputerChoice());
+    winner(result);
+    updateDisplay()
+})
+
+const computerDisplayedScore = document.createElement('p');
+const humanDisplayedScore = document.createElement('p');
+resultDiv.appendChild(computerDisplayedScore);
+resultDiv.appendChild(humanDisplayedScore);
 
 
    
---
+++ thoughts.md
@@ -0,0 +1,13 @@
+- remove logic that plays 5 rounds and replace it with buttons
+- create events for each button 
+- old code is a bit confusing so we will remove it doesn't work with what exists
+    - getComputerChoice() function remains as is
+    - human choice this function was removed it's serves no purpose now
+    - logic for play round 
+    - winner determination while winner is still gauged the same way the code needs to b    adjusted slightly 
+    - already have variables for the respecitive scores 
+    - create function that will take the result from calling play round and append
+        repective score
+    -DOM 
+        - Output the winner
+    
